0627d9269264111ad5d3826786965d3b8fba3070
Danish Ansari <dvansari360@gmail.com>
2025-06-15 15:18:11 +0530

    product controller implemented

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import AsyncHandler from "../utils/asyncHandler"
 import { newProductTypes } from "../types/product"
 import ApiError from "../utils/errorHanlder"
 import { Product } from "../models/product.model"
 import imageKit from "../utils/imageKit"
 import fs from "fs"
+import { baseQuery } from "../types/types"
 
 export const newProduct = AsyncHandler(async(req:Request<{},{},newProductTypes>,res:Response)=>{
     console.log("req.body",req.body)
@@ -30,7 +31,7 @@ export const newProduct = AsyncHandler(async(req:Request<{},{},newProductTypes>,
                 price,
                 ratings,
                 numberOfRating,
-                category,
+                category:category.toLowerCase(),
                 photo:uploadedPhoto.url
             })
             return res
@@ -148,3 +149,76 @@ export const deleteProduct = AsyncHandler(async(
         success: true
     })
 })
+
+export const filterProduct = AsyncHandler( async (req:Request,res:Response)=>{
+    const {category,price,search,sort} = req.query
+
+    const page =  Number(req.query.page) || 1;
+    const limit = Number( process.env.PRODUCT_PER_PAGE) || 8
+    const skip  = (page-1)*limit
+
+    const basequery:baseQuery = {}
+
+    if(category && typeof category === "string") basequery.category = category;
+
+    if(price && !isNaN(Number(price))) basequery.price = 
+        {
+            $lte:Number(price)
+        };
+
+    if(search && typeof search === "string") basequery.search = 
+        { 
+            $regex:String(search),
+            $options:"i"
+        };
+
+    let sortOptions : any = {}
+    if(sort === "asc"){
+        sortOptions.price = 1
+    }else{
+        sortOptions.price = -1
+    }
+    const sortedProducts = await Product.find(basequery)
+    .sort(sortOptions)
+    .limit(limit)
+    .skip(skip)
+    
+    const [allProducts,filteredOnlyProducts] = await  Promise.all([
+        sortedProducts,
+        Product.find(basequery)
+    ])
+    const products = allProducts
+    const totalPage = Math.ceil(filteredOnlyProducts.length/limit)
+    return res
+    .status(200)
+    .json({
+        message:"filtered successfully",
+        success:true,
+        products,
+        totalPage
+    })
+})
+
+export const getAllProducts = AsyncHandler(async (req: Request, res: Response) => {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
+    const skip = (page - 1) * limit;
+
+    const [products, totalProducts] = await Promise.all([
+        Product.find()
+            .limit(limit)
+            .skip(skip),
+        Product.countDocuments()
+    ]);
+
+    const totalPages = Math.ceil(totalProducts / limit);
+
+    return res.status(200).json({
+        message: "Products fetched successfully",
+        success: true,
+        products,
+        totalPages,
+        currentPage: page,
+        totalProducts
+    });
+});
