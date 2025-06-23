d97efea5a80a14d4cdf298e20ed4d8666e01618f
Danish Ansari <dvansari360@gmail.com>
2025-06-23 06:15:17 +0530

    ...

---
+++ frontend/src/redux/reducer/userReducer.ts
@@ -0,0 +1,26 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
+import type { userReducerInitialState } from "../../types/userReducer-types"
+import type { User } from "../../types/types"
+
+
+const initialState:userReducerInitialState = {
+    user:null!,
+    loading:true
+}
+
+export const userReducer = createSlice({
+    name:"userReducer",
+    initialState,
+    reducers:{
+        userExist : (state,action:PayloadAction<User>)=>{
+                state.loading = false
+                state.user = action.payload
+        },
+        userNotExist : (state)=>{
+            state.loading = false,
+            state.user = null!
+        }
+    }
+})
+
+export const {userExist,userNotExist} = userReducer.actions
---
+++ frontend/src/types/userReducer-types.ts
@@ -0,0 +1,6 @@
+import type { User } from "./types";
+
+export interface userReducerInitialState{
+    user:User,
+    loading:boolean
+}
