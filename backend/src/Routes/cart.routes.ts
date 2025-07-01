683328d793ecde66679860c0a74cfc902ae2812c
Danish Ansari <dvansari360@gmail.com>
2025-07-01 10:52:24 +0530

    ..

--- backend/src/Routes/cart.routes.ts
+++ backend/src/Routes/cart.routes.ts
@@ -1,9 +1,9 @@
 import { verifyJwt } from "../middlewares/authMiddleWare";
 import { Router } from "express";
-import {createCart} from "../controllers/cart.controller"
+import {createCart,decreaseProductQuantity} from "../controllers/cart.controller"
 
 
 
 export const cartRouter = Router()
 cartRouter.route("/create-cart/:productId").post(verifyJwt,createCart);
-
+cartRouter.route("/decrease-quantity").post(verifyJwt,decreaseProductQuantity);
