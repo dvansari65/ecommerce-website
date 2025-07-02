dde8b3e9ed0fb3b0a9f1c6fa50791e2d254d79d1
Danish Ansari <dvansari360@gmail.com>
2025-07-02 15:39:51 +0530

    init

--- backend/src/Routes/cart.routes.ts
+++ backend/src/Routes/cart.routes.ts
@@ -1,9 +1,11 @@
 import { verifyJwt } from "../middlewares/authMiddleWare";
 import { Router } from "express";
-import {createCart,decreaseProductQuantity} from "../controllers/cart.controller"
+import {createCart,decreaseProductQuantity,getAllCartProducts,deleteCart} from "../controllers/cart.controller"
 
 
 
 export const cartRouter = Router()
 cartRouter.route("/create-cart/:productId").post(verifyJwt,createCart);
-cartRouter.route("/decrease-quantity").post(verifyJwt,decreaseProductQuantity);
+cartRouter.route("/decrease-quantity/:productId").post(verifyJwt,decreaseProductQuantity);
+cartRouter.route("/get-cart-products").get(verifyJwt,getAllCartProducts);
+cartRouter.route("/deleteCart/").delete(verifyJwt,deleteCart);
