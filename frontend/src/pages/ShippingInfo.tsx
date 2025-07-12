855e88a54b20cb467cd9801ef203082e652b8dbb
Danish Ansari <dvansari360@gmail.com>
2025-07-12 11:47:18 +0530

    improve some things

--- frontend/src/pages/ShippingInfo.tsx
+++ frontend/src/pages/ShippingInfo.tsx
@@ -5,99 +5,129 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-function PlaceOrderFromCart() {
-  const dispatch  = useDispatch()
+function ShippingInfo () {
+  const dispatch = useDispatch()
+  const [message,setMessage] = useState("")
   const navigate = useNavigate()
-  const [formData,setFormData] = useState<shippingInfo>({
+  const [formData, setFormData] = useState<shippingInfo>({
     state: "",
-    pinCode:1212,
+    pinCode: 1212,
     address: "",
     city: "",
-    country:"",
+    country: "",
   })
 
-  const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
-      e.preventDefault();
-      dispatch(setShippingInfo(formData))
-      navigate("/create-payment")
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+   if(formData){
+    dispatch(setShippingInfo(formData))
+    navigate("/create-payment")
+   }else{
+      setMessage("all fields are required!")
+   }
   }
 
   return (
-    <div className='w-full h-screen bg-[#0f0c29] flex justify-center items-start mt-20 '>
-      <form onSubmit={handleSubmit}>
-       <Input 
-       label='ADDRESS'
-        value={formData.address} 
-        placehHolder='address..' 
-        onChange={(e)=>setFormData(prev=>(
-        {
-          ...prev,
-          address:e.target.value
-        }
-       ))}
-       id='address'
-       type='text'
-       htmlFor="address"
+    <div className='w-full h-screen bg-[#0f0c29] flex justify-center items-center flex-col gap-2  '>
+      <span className='text-red-400 text-[13px]'>{message}</span>
+      <form className='flex justify-center items-start  border-[1px] p-6 border-color flex-col gap-2 mb-20' onSubmit={handleSubmit}>
+        <Input
+          required={true}
+          label='ADDRESS'
+          value={formData.address}
+          placehHolder='address..'
+          onChange={(e) => {
+            setFormData(prev => (
+              {
+                ...prev,
+                address: e.target.value
+              }
+            ))
+            setMessage("")
+          }}
+          id='address'
+          type='text'
+          htmlFor="address"
         />
-        <Input 
-            label='CITY'
-            value={formData.city} 
-            placehHolder='e.g PUNE' 
-            onChange={(e)=>setFormData(prev=>(
-            {
-              ...prev,
-              city:e.target.value
-            }
-            ))}
-            id='city'
-            type='text'
-            htmlFor="city"
+        <Input
+        required={true}
+          label='CITY'
+          value={formData.city}
+          placehHolder='e.g PUNE'
+          onChange={(e) => {
+            setFormData(prev => (
+              {
+                ...prev,
+                city: e.target.value
+              }
+            ))
+            setMessage("")
+          }}
+          id='city'
+          type='text'
+          htmlFor="city"
         />
-        <Input 
-            label='STATE'
-            value={formData.state} 
-            placehHolder='e.g MAHARASHTRA' 
-            onChange={(e)=>setFormData(prev=>(
-            {
-              ...prev,
-              state:e.target.value
-            }
-            ))}
-            id='state'
-            type='text'
-            htmlFor="state"
+        <Input
+        required={true}
+          label='STATE'
+          value={formData.state}
+          placehHolder='e.g MAHARASHTRA'
+          onChange={(e) => {
+            setFormData(prev => (
+              {
+                ...prev,
+                state: e.target.value
+              }
+            ))
+            setMessage("")
+          }}
+          id='state'
+          type='text'
+          htmlFor="state"
         />
-        <Input 
-            label='COUNTRY'
-            value={formData.country} 
-            placehHolder='e.g INDIA' 
-            onChange={(e)=>setFormData(prev=>(
-            {
-              ...prev,
-              country:e.target.value
-            }
-            ))}
-            id='country'
-            type='text'
-            htmlFor="country"
+        <Input
+        required={true}
+          label='COUNTRY'
+          value={formData.country}
+          placehHolder={"e.g. INDIA"}
+          onChange={(e) => {
+            setFormData(prev => (
+              {
+                ...prev,
+                country: e.target.value
+              }
+            ))
+            setMessage("")
+          }}
+          id='country'
+          type='text'
+          htmlFor="country"
         />
-        <Input 
-            label='PINCODE'
-            value={formData.pinCode} 
-            placehHolder='0000' 
-            onChange={(e)=>setFormData(prev=>(
-            {
-              ...prev,
-              country:e.target.value
-            }
-            ))}
-            id='country'
-            type='number'
-            htmlFor="country"
+        <Input
+         required={true}
+          label='PINCODE'
+          value={formData.pinCode}
+          placehHolder='0000'
+          onChange={(e) => {
+            setFormData(prev => (
+              {
+                ...prev,
+                address: e.target.value
+              }
+            ))
+            setMessage("")
+          }}
+          id='pinCode'
+          type='number'
+          htmlFor="pinCode"
         />
+        <div className='mt-3 w-full flex justify-center items-center '>
+          <button className='p-3 w-[170px] bg-[#0f0c29] border border-transparent 
+              hover:border-[#322d5e] hover:cursor-pointer  rounded-[3px] transition-all duration-100 ease-in-out'>SUBMIT</button>
+        </div>
       </form>
     </div>
   )
 }
 
-export default PlaceOrderFromCart
+export default ShippingInfo
