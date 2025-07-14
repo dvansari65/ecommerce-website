a35144bfadbf1a7a50378785a1317b9c3f19a4d8
Danish Ansari <dvansari360@gmail.com>
2025-07-14 06:55:26 +0530

    create reducer for cart

--- frontend/src/redux/reducer/cartReducer.ts
+++ frontend/src/redux/reducer/cartReducer.ts
@@ -1,13 +1,23 @@
-import type { shippingInfo } from "@/types/api-types";
+import ShippingInfo from "@/pages/ShippingInfo";
+import type { orderReponse } from "@/types/api-types";
 import { createSlice } from "@reduxjs/toolkit";
 
 
-const initialState:shippingInfo = {
-    address:"",
-    state:"",
-    country:"",
-    pinCode:0,
-    city:""
+const initialState:orderReponse = {
+    shippingCharges:0,
+    total:0,
+    tax:0,
+    subtotal:0,
+    discount:0,
+    shippingInfo:{
+        address:"",
+        pinCode:0,
+        state:"",
+        city:"",
+        country:""
+    },
+    orderItems:[]
+
 }
 
 export const cartReducer = createSlice({
@@ -16,11 +26,28 @@ export const cartReducer = createSlice({
     reducers:{
         saveShippingInfo : (State,action)=>{
             const {address,state,country,pinCode,city} = action.payload
-            State.address = address,
-            State.state = state,
-            State.country = country,
-            State.pinCode = pinCode,
-            State.city = city
+            State.shippingInfo.address = address,
+            State.shippingInfo.state= state,
+            State.shippingInfo.country = country,
+            State.shippingInfo.pinCode = pinCode,
+            State.shippingInfo.city = city
+        },
+        saveNumericalData : (state,action)=>{
+            state.discount = action.payload
+            state.tax = action.payload
+            state.total = action.payload
+            state.subtotal = action.payload
+            state.shippingCharges = action.payload
+        },
+        saveOrderItems : (state,action)=>{
+            const {name,photo,price,quantity,productId} = action.payload
+            state.orderItems.push({
+                name,
+                photo,
+                price,
+                quantity,
+                productId
+            })
         }
     }
 })
