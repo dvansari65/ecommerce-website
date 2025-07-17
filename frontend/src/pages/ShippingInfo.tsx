2f1f2449d9f243edc18650941360356a82066ef2
Danish Ansari <dvansari360@gmail.com>
2025-07-17 16:37:40 +0530

    fix bugs in shippinginfo

--- frontend/src/pages/ShippingInfo.tsx
+++ frontend/src/pages/ShippingInfo.tsx
@@ -1,187 +1,207 @@
-import Input from '@/components/features/Input'
-import type { createPaymentResponse, productTypeFromOrder, shippingInfo } from '@/types/api-types'
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import axios from "axios"
-import { saveNumericalData, saveOrderItems, saveShippingInfo } from '@/redux/reducer/cartReducer'
-import CheckCoupon from '@/components/features/CreateCoupon'
-import { server } from '@/config/constants'
-import type { RootState } from '@/redux/reducer/store'
-import Spinner from '@/components/ui/LoaderIcon'
-import toast from 'react-hot-toast'
-
+import Input from "@/components/features/Input";
+import type {
+  createPaymentResponse,
+  productTypeFromOrder,
+  shippingInfo,
+} from "@/types/api-types";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import {
+  saveNumericalData,
+  saveOrderItems,
+  saveShippingInfo,
+} from "@/redux/reducer/cartReducer";
+import CheckCoupon from "@/components/features/CreateCoupon";
+import { server } from "@/config/constants";
+import type { RootState } from "@/redux/reducer/store";
+import Spinner from "@/components/ui/LoaderIcon";
+import toast from "react-hot-toast";
 
 function ShippingInfo() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [error,setError] = useState(false)
-  const dispatch = useDispatch()
-  const { code } = useSelector((state: RootState) => state.couponReducer)
-  const [message, setMessage] = useState("")
-  const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+  const { code } = useSelector((state: RootState) => state.couponReducer);
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
   const [formData, setFormData] = useState<shippingInfo>({
     state: "",
     pinCode: 1212,
     address: "",
     city: "",
     country: "",
-  })
-
+  });
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setIsLoading(true)
-    dispatch(saveShippingInfo(formData))
-   
+    setIsLoading(true);
+    dispatch(saveShippingInfo(formData));
+    console.log("code:", code);
     try {
       const endpoint = code
         ? `${server}/api/v1/coupon/create-payment-from-cart?code=${code}`
         : `${server}/api/v1/coupon/create-payment-from-cart`;
-
-      const { data }: { data: createPaymentResponse } = await axios.post(endpoint, {
-        shippingInfo: formData
-      },
+        console.log("code:", code);
+      const { data }: { data: createPaymentResponse } = await axios.post(
+        endpoint,
         {
-          withCredentials: true
+          shippingInfo: formData,
+        },
+        {
+          withCredentials: true,
         }
-      )
-      
-      let items: productTypeFromOrder[] = data.cart.items.map(i => ({
+      );
+      console.log("data:", data);
+      let items: productTypeFromOrder[] = data.cart.items.map((i) => ({
         name: i.productId?.name || "",
         photo: i.productId?.photo || "",
         price: i.productId?.price || 0,
         quantity: i.quantity || 1,
-        productId: i.productId?._id || ""
+        productId: i.productId?._id || "",
       }));
-      dispatch(saveOrderItems(items))
+      dispatch(saveOrderItems(items));
 
-      dispatch(saveNumericalData({
-        subtotal: data.subtotal,
-        tax: data.tax,
-        shippingCharges: data.shippingCharges,
-        discount: data.discount,
-        total: data.total
-      }))
-
-      setIsLoading(false)
-
+      dispatch(
+        saveNumericalData({
+          subtotal: data.subtotal,
+          tax: data.tax,
+          shippingCharges: data.shippingCharges,
+          discount: data.discount,
+          total: data.total,
+        })
+      );
+      navigate("/payment",{
+        state:data?.clientSecret
+      })
+      setIsLoading(false);
     } catch (error: any) {
-      setIsLoading(false)
-      setError(error.message || "failed to proceed! Please visit product page!")
-      console.log(error)
-      toast.error(error?.data?.message || error.message  ||  "failed to submit data!")
+      setIsLoading(false);
+      setError(
+        error.message || "failed to proceed! Please visit product page!"
+      );
+      console.log(error);
+      toast.error(
+        error?.data?.message || error.message || "failed to submit data!"
+      );
     }
-  }
-  if (isLoading) return <div className='w-full h-screen flex justify-center items-center '><Spinner /></div>
-  if (error) return <div className='w-full h-screen flex justify-center items-center text-red-400 '>{error} !!!</div>
+  };
+  if (isLoading)
+    return (
+      <div className="w-full h-screen flex justify-center items-center ">
+        <Spinner />
+      </div>
+    );
+  if (error)
+    return (
+      <div className="w-full h-screen flex justify-center items-center text-red-400 ">
+        {error} !!!
+      </div>
+    );
 
   return (
-    <div className='h-screen w-full' >
-      <div className='grid grid-cols-2 '>
-        <div className='col-span-1 flex flex-col justify-center items-end h-[80vh] mr-5'>
+    <div className="h-screen w-full">
+      <div className="grid grid-cols-2 ">
+        <div className="col-span-1 flex flex-col justify-center items-end h-[80vh] mr-5">
           <span>{message}</span>
-          <form className='p-4  w-[60vh] h-full flex flex-col justify-center items-center gap-2 bg-transparent border-[1px] border-color ' onSubmit={handleSubmit}>
+          <form
+            className="p-4  w-[60vh] h-full flex flex-col justify-center items-center gap-2 bg-transparent border-[1px] border-color "
+            onSubmit={handleSubmit}
+          >
             <Input
               required={true}
-              label='ADDRESS'
+              label="ADDRESS"
               value={formData.address}
-              placehHolder='address..'
+              placehHolder="address.."
               onChange={(e) => {
-                setFormData(prev => (
-                  {
-                    ...prev,
-                    address: e.target.value
-                  }
-                ))
-                setMessage("")
+                setFormData((prev) => ({
+                  ...prev,
+                  address: e.target.value,
+                }));
+                setMessage("");
               }}
-              id='address'
-              type='text'
+              id="address"
+              type="text"
               htmlFor="address"
             />
             <Input
               required={true}
-              label='CITY'
+              label="CITY"
               value={formData.city}
-              placehHolder='e.g PUNE'
+              placehHolder="e.g PUNE"
               onChange={(e) => {
-                setFormData(prev => (
-                  {
-                    ...prev,
-                    city: e.target.value
-                  }
-                ))
-                setMessage("")
+                setFormData((prev) => ({
+                  ...prev,
+                  city: e.target.value,
+                }));
+                setMessage("");
               }}
-              id='city'
-              type='text'
+              id="city"
+              type="text"
               htmlFor="city"
             />
             <Input
               required={true}
-              label='STATE'
+              label="STATE"
               value={formData.state}
-              placehHolder='e.g MAHARASHTRA'
+              placehHolder="e.g MAHARASHTRA"
               onChange={(e) => {
-                setFormData(prev => (
-                  {
-                    ...prev,
-                    state: e.target.value
-                  }
-                ))
-                setMessage("")
+                setFormData((prev) => ({
+                  ...prev,
+                  state: e.target.value,
+                }));
+                setMessage("");
               }}
-              id='state'
-              type='text'
+              id="state"
+              type="text"
               htmlFor="state"
             />
             <Input
               required={true}
-              label='COUNTRY'
+              label="COUNTRY"
               value={formData.country}
               placehHolder={"e.g. INDIA"}
               onChange={(e) => {
-                setFormData(prev => (
-                  {
-                    ...prev,
-                    country: e.target.value
-                  }
-                ))
-                setMessage("")
+                setFormData((prev) => ({
+                  ...prev,
+                  country: e.target.value,
+                }));
+                setMessage("");
               }}
-              id='country'
-              type='text'
+              id="country"
+              type="text"
               htmlFor="country"
             />
             <Input
               required={true}
-              label='PINCODE'
+              label="PINCODE"
               value={formData.pinCode}
-              placehHolder='0000'
+              placehHolder="0000"
               onChange={(e) => {
-                setFormData(prev => (
-                  {
-                    ...prev,
-                    pinCode: Number(e.target.value)
-                  }
-                ))
-                setMessage("")
+                setFormData((prev) => ({
+                  ...prev,
+                  pinCode: Number(e.target.value),
+                }));
+                setMessage("");
               }}
-              id='pinCode'
-              type='number'
+              id="pinCode"
+              type="number"
               htmlFor="pinCode"
             />
-            <div className='w-full flex justify-center items-center mt-4 '>
-              <button className='text-white w-[150px] border-[1px] border-[#322d5e]  p-2 rounded-[4px] hover:border-[#ae90f4] transition-all duration-200 ease-in-out '>SUBMIT</button>
+            <div className="w-full flex justify-center items-center mt-4 ">
+              <button className="text-white w-[150px] border-[1px] border-[#322d5e]  p-2 rounded-[4px] hover:border-[#ae90f4] transition-all duration-200 ease-in-out ">
+                SUBMIT
+              </button>
             </div>
           </form>
         </div>
-        <div className='col-span-1 flex flex-col justify-center items-center'>
+        <div className="col-span-1 flex flex-col justify-center items-center">
           <CheckCoupon />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ShippingInfo
+export default ShippingInfo;
