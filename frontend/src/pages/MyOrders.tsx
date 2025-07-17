04bb92eabf95e3336bdd858fa6a49deb2d711586
Danish Ansari <dvansari360@gmail.com>
2025-07-17 11:35:38 +0530

    created my order page

--- frontend/src/pages/MyOrders.tsx
+++ frontend/src/pages/MyOrders.tsx
@@ -1,36 +1,51 @@
 import Button from "@/components/features/Button";
 import OrderCard from "@/components/features/OrderCard";
+import Spinner from "@/components/ui/LoaderIcon";
 import { useMyOrderQuery } from "@/redux/api/orderApi";
 import { useEffect, useState } from "react";
 
+
 function MyOrders() {
   const [page, setPage] = useState<number>(1);
-  const { getMyOrders, isError, isLoading } = useMyOrderQuery({});
+  const { data, isError, isLoading } = useMyOrderQuery({ page });
+ 
+  const allOrders = data?.orders
+ 
+
   const handleIncrease = async () => {};
   const handleDecrease = async () => {};
+  useEffect(() => {
+    console.log("data:", allOrders);
+    
+  }, [data]);
+
+  if (isLoading) return <Spinner />;
+  if (isError) return <div className="text-gray-400 ">something went wrong!</div>;
+
   return (
-    <div className=" h-screen bg-transparent grid grid-cols-12">
+    <div className=" h-screen bg-transparent grid grid-cols-13  ">
       <div className="col-span-8">
-        <div className=" flex flex-col ">
-          <span className="mb-3 font-extralight text-gray-500">ORDERS:</span>
-          <div>
-            <OrderCard
-              name=""
-              photo=""
-              quantity={2}
-              increaseQuantity={handleIncrease}
-              decreaseQuantity={handleDecrease}
-              category=""
-              status=""
-            />
-          </div>
-        </div>
-        <div>
-          
+       {
+        allOrders?.map(order=>(
+          <OrderCard 
+          shippingCharges={order.shippingCharges} 
+          total={order.total} 
+          discount={order.discount} 
+          status={order.status} 
+          tax={order.tax}
+          createdAt={order.createdAt}
+          orderItems={order.orderItems}
+          increaseQuantity={handleIncrease}
+          decreaseQuantity={handleDecrease}
+          />
+        ))
+       }
+        <div className="w-full flex flex-row justify-center items-center gap-3">
+          <Button title="prev" onClick={()=>setPage(prev=>prev-1)} disabled={page === 1} />
+            <span>{page} of {data?.totatPages || 1}</span>
+          <Button title="next" onClick={()=>{}} disabled={page === data?.totatPages} />
         </div>
       </div>
-
-      <div className="col-span-4"></div>
     </div>
   );
 }
